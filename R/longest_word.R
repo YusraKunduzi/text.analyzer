c7c83b9fcdb819c4ce2c5517a116e3633be292a4
YusraKunduzi <yusra.kunduzi@student.hu.nl>
2024-12-28 17:47:13 +0100

    nog 2 functies

---
+++ R/longest_word.R
@@ -0,0 +1,28 @@
+longest_words <- function(text) {
+  #make lines out of the text
+  lines <- unlist(strsplit(text, "\n"))
+  #make objects
+  longest_word <-  " "
+  longest_length <- 0
+  place <-  NULL
+
+  #loop for each word
+  for (line in seq_along(lines)) {
+    words <- unlist(strsplit(lines[line_number], "\\s+"))
+    #count the length of each word
+    for (word in words) {
+      word_length <- nchar(word)
+
+      #checks if current word is longer than the previous longest word
+      if (word_length > longest_length) {
+        longest_word <- word
+        longest_length <- word_length
+        place <- list(line = line_number, index = which(words == word))
+      }
+    }
+  }
+
+  #list of longest word with length and position
+  list(word = longest, length = longest_length, position = place)
+
+}
